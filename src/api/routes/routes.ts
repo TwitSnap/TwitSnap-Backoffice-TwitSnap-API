d7b202cb8e70ded83caf9ecf6fa7e9cb202d9ec6
Guido Menendez <guidomenendez0@gmail.com>
2024-11-26 06:59:38 -0300

    update routes

--- src/api/routes/routes.ts
+++ src/api/routes/routes.ts
@@ -4,13 +4,13 @@ import {twitSnapController} from "../../utils/container/container";
 const router = Router();
 
 // ? Twits
-router.get("/v1/twits", twitSnapController.getTwits);
-router.get("/v1/twits/:twitId", twitSnapController.getTwit);
-router.post("/v1/twits/:twitId/block", twitSnapController.blockOrUnblockTwit);
+router.get("/v1/ts/twits", twitSnapController.getTwits);
+router.get("/v1/ts/twits/:twitId", twitSnapController.getTwit);
+router.post("/v1/ts/twits/:twitId/block", twitSnapController.blockOrUnblockTwit);
 
 // ? Users
-router.post("/v1/users", twitSnapController.getUsers);
-router.get("/v1/users/:userId", twitSnapController.getUser);
-router.post("/v1/users/:userId/ban", twitSnapController.banOrUnbanUser);
+router.post("/v1/ts/users", twitSnapController.getUsers);
+router.get("/v1/ts/users/:userId", twitSnapController.getUser);
+router.post("/v1/ts/users/:userId/ban", twitSnapController.banOrUnbanUser);
 
 export default router;

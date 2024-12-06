d7bb3c5cdb3aa291a5c363c65fc05a4fa945260f
Guido Menendez <guidomenendez0@gmail.com>
2024-12-06 07:31:53 -0300

    Add routes layer

--- src/api/routes/routes.ts
+++ src/api/routes/routes.ts
@@ -14,4 +14,10 @@ router.get("/v1/ts/users/:userId", twitSnapController.getUser);
 router.post("/v1/ts/users/:userId/ban", twitSnapController.banOrUnbanUser);
 router.get("/v1/ts/users/metrics", twitSnapController.getMetrics);
 
+// ? Service registry
+router.get("/v1/ts/service", twitSnapController.getServices);
+router.post("/v1/ts/service", twitSnapController.createService);
+router.get("/v1/ts/service/:id", twitSnapController.getService);
+router.post("/v1/ts/service/status", twitSnapController.changeServiceStatus);
+
 export default router;

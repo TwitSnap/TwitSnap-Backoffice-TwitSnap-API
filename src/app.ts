9ef24c6e925eb7c668f58f4f2b515d1d3e921914
Guido Menendez <guidomenendez0@gmail.com>
2024-12-06 07:36:56 -0300

    fix cors

--- src/app.ts
+++ src/app.ts
@@ -6,17 +6,9 @@ import {logger} from "./utils/container/container";
 import {PORT} from "./utils/config";
 import swaggerUi from "swagger-ui-express";
 import swaggerDocument from './utils/swagger/docs/swaggerDocs.json';
-import cors, {CorsOptions} from "cors";
 
 const app = express();
 
-const corsOptions: CorsOptions = {
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-};
-
-app.use(cors(corsOptions));
 app.use(express.json());
 app.use(router)
 app.use(errorMiddleware);
